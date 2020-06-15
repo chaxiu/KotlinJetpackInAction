acae93b8ec06820a0ad5bc4e770a6a5390061e36
zhu.tao <zhu.tao@immomo.com>
2020-06-15 16:11:50 +0800

    Fix error after rename build.gradle to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:4.0.0"
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        classpath("com.android.tools.build:gradle:4.0.0")
     }
 }
 
@@ -19,6 +16,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
