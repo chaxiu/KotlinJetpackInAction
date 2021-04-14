334097ce018381da65cb6eaf0a682b4e92de15e3
zhu.tao <zhu.tao@immomo.com>
2021-04-14 16:18:47 +0800

    Modify: update project settings.

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -6,9 +6,9 @@ object Versions {
     const val espresso = "3.2.0"
     const val lifecycle = "2.2.0"
 
-    const val kotlinVersion = "1.4.0-rc"
+    const val kotlinVersion = "1.4.32"
     const val ktxCore = "1.2.0"
-    const val coroutines = "1.3.8-1.4.0-rc"
+    const val coroutines = "1.4.3"
 
     const val volley = "1.1.1"
     const val retrofit = "2.9.0"
